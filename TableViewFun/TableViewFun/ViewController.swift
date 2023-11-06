7a7f5898db3e2ec96aa3e6264225d6085776d6f8
akabishau <aleksey.kabishau@icloud.com>
2023-11-06 16:23:08 -0500

    create project

---
+++ TableViewFun/TableViewFun/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  TableViewFun
+//
+//  Created by Aleksey Kabishau on 11/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+	}
+}
+
