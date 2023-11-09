815cc0363826fe928d85b12865ef8a251c3ae750
akabishau <aleksey.kabishau@icloud.com>
2023-11-09 13:04:05 -0500

    starting no-storyboard code

---
+++ Onboarding/Onboarding/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Onboarding
+//
+//  Created by Aleksey Kabishau on 11/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		view.backgroundColor = .systemBackground
+	}
+}
+
